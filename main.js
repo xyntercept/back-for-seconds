703090c640ff8211985b0dbeddc0a0221b1c1464
xyntercept <ramenoodlez10@gmail.com>
2025-11-17 20:20:23 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+Game.registerMod("BackForSeconds", {
+  init: function() {
+    Game.BankAchievement("Back for seconds");
+    Game.BankAchievement("Unstoppable force");
+    Game.BankAchievement("Gastronomical unit");
+  }
+})
