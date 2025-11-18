2c0b10d1894919e208fbc1022bf31fdf1089b75f
xyntercept <ramenoodlez10@gmail.com>
2025-11-18 10:02:04 +0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ Game.registerMod("BackForSeconds", {
   const customStyle = document.createElement('style')
   customStyle.type = 'text/css'
   customStyle.textContent = `
-  .crate
+  .achievement
   {
     image-rendering: optimizeSpeed;             /* Older versions of FF          */
     image-rendering: -moz-crisp-edges;          /* FF 6.0+                       */
